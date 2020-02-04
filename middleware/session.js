b10bd2828afccb10ac6b7e8580d1e194c578cb24
Robert Gordon <Komeuppance@gmail.com>
2020-02-04 11:37:45 -0800

    Updated cookies to check for boolean instead of email

--- middleware/session.js
+++ middleware/session.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-    console.log('Req session', req.session, 'email', req.session.email )
-    if(req.session.loggedIn && req.session.email) {
+    console.log('Req session', req.session.loggedIn)
+    if(req.session.loggedIn) {
         next();
     } else {
         res.status(403).json( {message: 'Unauthorized access!' });
